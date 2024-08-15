8b7dc34cffb4f78b94159d6ef91209aab79e4601
GeovaniX <geovanisantos019@gmail.com>
2024-08-15 20:34:20 -0300

    first commit

---
+++ src/main/java/geovani/training/traininggs/TraininggsApplication.java
@@ -0,0 +1,13 @@
+package geovani.training.traininggs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraininggsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraininggsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geovani/training/traininggs/TraininggsApplicationTests.java
@@ -0,0 +1,13 @@
+package geovani.training.traininggs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraininggsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
